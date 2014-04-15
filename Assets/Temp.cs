0e0daf8aa9d7af48e27329e8929a7a5c57b8907a
Watzlaw <Martin.Brocklemann@ur.de>
2014-04-15 17:17:33 +0200

    Pizza ini

---
+++ Assets/Temp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Temp : MonoBehaviour {
+
+	public float speed = 0;
+	public Transform target =null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards(transform.position, target.position, Time.deltaTime * speed);
+	}
+}
--- Assets/pizza_force.cs
+++ Assets/pizza_force.cs
@@ -4,11 +4,15 @@ using System.Collections;
 public class pizza_force : MonoBehaviour {
 
     //Rigidbody bullet = new Rigidbody();
-    public int speed = 20;
+    public float speed = 500f;
     public GameObject bullet = null;
+	public GameObject shot;
+	public GameObject vater = null;
 
 	// Use this for initialization
 	void Start () {
+
+		moveOnSpawn();
 	
 	}
 	
@@ -17,16 +21,35 @@ public class pizza_force : MonoBehaviour {
 
         if (Input.GetButtonDown("Fire1"))
         {
+
             shoot();
         }
 	
 	}
 
-    void shoot()
+    
+	void shoot()
     {
-        GameObject shot = GameObject.Instantiate(bullet, transform.position, transform.rotation) as GameObject;
-        shot.rigidbody.AddForce(transform.forward * 500);
+		//Debug.Log("hit");
+        //Rigidbody shot = Instantiate(bullet, transform.position, transform.rotation) as Rigidbody;
+		//shot.AddForce(transform.forward * 500);
+		//shot.velocity = transform.TransformDirection(Vector3(0,0,500));
+
+
+		shot.transform.parent = null;
+		shot.rigidbody.useGravity = true;
+		shot.rigidbody.AddForce(transform.forward*500);
     }
+
+	void moveOnSpawn()
+	{
+		shot = Instantiate(bullet, transform.position, transform.rotation) as GameObject;
+		shot.rigidbody.Sleep();
+		shot.transform.parent = vater.transform;
+		shot.transform.Rotate(-90,0,0);
+		shot.transform.Translate (0,3,0);
+		//shot.transform.Translate (Vector3.forward * Time.deltaTime);
+	}
 
     //void OnMouseDown()
     //{
